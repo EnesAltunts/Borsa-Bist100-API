d374d86d02f217965abc276ae65de9da0bc2e86a
Enes Akın Altuntaş <enesaltunts@gmail.com>
2023-12-05 13:19:55 +0300

    BIST100 Hisse Verileri

---
+++ BIST100/Borsa_BIST100/Borsa_BIST100/Controllers/BorsaController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Borsa_BIST100.Controllers
+{
+    public class BorsaController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            // API'den veri çekme
+            var apiUrl = "https://localhost:44386/api/borsa";
+
+            using (var httpClient = new HttpClient())
+            {
+                string response = await httpClient.GetStringAsync(apiUrl);
+
+
+                // JObject json = JObject.Parse(response);
+
+                JArray arr = JArray.Parse(response);
+
+                List<string> hisseList = new List<string>();
+                List<string> fiyatList = new List<string>();
+                List<string> degisimList = new List<string>();
+                List<string> saatDeger = new List<string>();
+
+                for (int i = 0; i < arr.Count; i += 5)
+                {
+                    hisseList.Add(arr[i].ToString());
+                    fiyatList.Add(arr[i + 2].ToString());
+                    degisimList.Add(arr[i + 3].ToString());
+                    saatDeger.Add(arr[i + 4].ToString());
+                }
+
+                ViewBag.HisseList = hisseList;
+                ViewBag.FiyatList = fiyatList;
+                ViewBag.DegisimList = degisimList;
+                ViewBag.SaatDeger = saatDeger;
+
+                return View();
+            }
+        }
+    }
+}
---
+++ BIST100/VeriCekme/VeriCekme/Controllers/BorsaController.cs
@@ -0,0 +1,43 @@
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace VeriCekme_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BorsaController : ControllerBase
+    {
+        [HttpGet]   
+        public async Task<IEnumerable<string>> Data()
+        {
+            // HTML içeriğini almak için HttpClient kullanılıyor
+            using (HttpClient client = new HttpClient())
+            {
+                // Sayfanın HTML içeriğini al
+                var htmlContent = await client.GetStringAsync("https://uzmanpara.milliyet.com.tr/canli-borsa/bist-100-hisseleri/");
+
+                // HTML içeriğini işlemek için HtmlWeb ve HtmlNodeCollection kullanılıyor
+                var web = new HtmlWeb();
+                var doc = new HtmlDocument();
+                doc.LoadHtml(htmlContent);
+
+                // HTML içinde belirli bir tabloyu seçmek için XPath kullanılıyor
+                var td = doc.DocumentNode.SelectNodes("//table[@class='table3']//tr//td");
+
+                // İşlenmiş verileri bir liste olarak döndür
+                if (td != null)
+                {
+                    return td.Select(item => item.InnerText).ToList();
+                }
+
+                return new List<string>(); // Eğer veri bulunamazsa boş liste döndür
+            }
+        }
+    }
+}
